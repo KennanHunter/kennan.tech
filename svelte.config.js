f65250b40b2949443c0e2b75e0c749220501ed2f
Kennan Hunter <kennanhunter5@gmail.com>
2022-07-02 16:05:18 -0400

    remark configs + css

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,8 @@
 import adapter from "@sveltejs/adapter-static";
-import preprocess from "svelte-preprocess";
-import image from "svelte-image";
 import { mdsvex } from "mdsvex";
 import rehypeSlug from "rehype-slug";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import image from "svelte-image";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,7 +14,7 @@ const config = {
 		mdsvex({
 			extensions: [".md"],
 			layout: "./src/lib/components/MarkdownLayout.svelte",
-			rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
+			rehypePlugins: [rehypeSlug],
 		}),
 		image(),
 	],
