97ea37a5f6c61b771e25af904174c3485b922e5e
Kennan Hunter <kennanhunter5@gmail.com>
2022-07-03 08:57:51 -0400

    more seo

--- src/lib/components/SEO.svelte
+++ src/lib/components/SEO.svelte
@@ -4,13 +4,16 @@
 	export let imageURL: string = "";
 	export let technologies: string[] = [];
 	export let keywords: string[] = [];
+	export let author: string = "Kennan Hunter";
 </script>
 
 <svelte:head>
 	<meta property="og:title" content={title} />
 	<title>{title}</title>
 	<meta property="og:description" content={description} />
+	<meta property="description" content={description} />
 	<meta property="og:image" content={imageURL} />
+	<meta name="author" content={author} />
 	<meta
 		name="keywords"
 		content={["Blog", "Programming"].concat(keywords).join(" ")}
