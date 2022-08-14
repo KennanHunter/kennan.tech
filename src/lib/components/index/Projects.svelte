c3b677a0626a2c85564341aeed938b42099b9c5b
Kennan Hunter <kennanhunter5@gmail.com>
2022-08-14 01:05:30 -0400

    add link redirect to projects

--- src/lib/components/index/Projects.svelte
+++ src/lib/components/index/Projects.svelte
@@ -48,7 +48,31 @@
 		<img
 			src="/assets/index/RedPandaLMSLogo.svg"
 			alt="test"
-			style="width: 30%"
+			style="height: 15em"
+		/>
+	</Project>
+
+	<Project
+		title="Link Shortener"
+		description="A simple link shortener I built 
+		for my subdomain a.kennan.tech. It uses CloudFlare Workers
+		and KV to deliver redirect responses at blazingly fast speeds"
+		links={[
+			{
+				text: "GitHub",
+				href: "https://github.com/KennanHunter/link",
+			},
+			{
+				text: "Blog Post",
+				href: "/blog/e/url-shortener-cloudflare",
+			},
+		]}
+		switchOnSmall="true"
+	>
+		<img
+			src="/assets/index/CloudflareWorkersLogo.png"
+			alt="Cloudflare Workers Logo"
+			style="margin:1em"
 		/>
 	</Project>
 </section>
