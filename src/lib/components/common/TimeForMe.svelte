f88fe3e6e7620a969d115cb06fd6a159aa6f43c2
Kennan Hunter <kennanhunter5@gmail.com>
2022-10-11 21:09:45 -0400

    Time for me component (maybe use later)

---
+++ src/lib/components/common/TimeForMe.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	const options = {
+		timeZone: "America/Indiana/Indianapolis",
+		weekday: "long",
+		hour: "numeric",
+		minute: "numeric",
+		hour12: true,
+	};
+
+	let timeText = new Date().toLocaleString("en-US", options as any);
+
+	setInterval(() => {
+		timeText = new Date().toLocaleString("en-US", options as any);
+	}, 1 * 1000);
+</script>
+
+<p>
+	It is currently {timeText} for me.
+</p>
